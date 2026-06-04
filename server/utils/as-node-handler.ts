146bc506757b86d303d3352e54e64351bab8f3c9
levos_snr <lewisodero27@gmail.com>
2026-06-04 18:25:19 +0300

    fix(typecheck): tsc --noEmit

--- server/utils/as-node-handler.ts
+++ server/utils/as-node-handler.ts
@@ -2,12 +2,11 @@ import { defineEventHandler, toRequest, type H3Event } from "nitro/h3";
 import type { IncomingMessage, ServerResponse } from "node:http";
 
 export type WebApiHandler = (request: Request) => Promise<Response>;
-
 export type NodeApiHandler = (req: IncomingMessage, res: ServerResponse) => void | Promise<void>;
 
 export function asWebHandler(handler: WebApiHandler) {
   return defineEventHandler(async (event: H3Event) => {
-    const request = toRequest(event);
+    const request = toRequest(event.node.req);
     return handler(request);
   });
 }
